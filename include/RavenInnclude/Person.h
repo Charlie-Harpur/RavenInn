8c4ec04700a9c49c7c253b407d3e9b562f09a6b7
Charlie_H <c.harpur@hotmail.com>
2023-01-26 16:43:39 -0500

    Added getters and setters + copy function to person class

--- include/RavenInnclude/Person.h
+++ include/RavenInnclude/Person.h
@@ -7,6 +7,10 @@ class Person
 {
     public:
         static const int INVENTORY_SIZE = 10;
+        static const int STATUS_EFFECT_NUM = 2;
+
+        bool *pStatusEffects;
+        int *pInventory;
 
         Person();
         ~Person();
@@ -14,17 +18,34 @@ class Person
         Person & operator=(const Person &rP);
 
         std::string getName() const;
-
+        int getAge() const;
+        bool getGender() const;
+        int getAllignment() const;
+        int getHappiness() const;
+        int getGold() const;
         int getOccupation() const;
+
+        void setName(std::string &rName);
+        void setAge(int &rAge);
+        void setGender(int &rGender);
+        void setAllignment(int &rAllignment);
+        void setHappiness(int &rHappiness);
+        void setGold(int &rGold);
         void setOccupation(const int &rNewOccupation);
 
-        int * getInventory() const;
-
     private:
+        static const int MAX_HAPPINESS = 3;
         std::string name;
 
+        int age;
+        bool gender;
+        int allignment;
+        int happiness;
+
+        int gold;
         int occupation;
-        int *pInventory;
+
+        void copyPerson(const Person &rP);
 };
 
 #endif
--- src/Person.cpp
+++ src/Person.cpp
@@ -1,19 +1,24 @@
 #include "RavenInnclude/Person.h"
 
-Person::Person(): name(""),
+Person::Person(): name("Mike Hawk"),
+    age(0),
+    gender(true),
+    allignment(0),
+    happiness(0),
+    pStatusEffects(new bool[STATUS_EFFECT_NUM]),
+    gold(0),
     occupation(0),
     pInventory(new int[INVENTORY_SIZE]) {}
 
 Person::~Person()
 {
+    delete[] pStatusEffects;
     delete[] pInventory;
 }
 
 Person::Person(const Person &rP)
 {
-    name = rP.name;
-    occupation = rP.occupation;
-    pInventory = new int[rP.INVENTORY_SIZE];
+    copyPerson(rP);
 }
 
 Person & Person::operator=(const Person &rP)
@@ -21,17 +26,18 @@ Person & Person::operator=(const Person &rP)
     if (this == &rP)
         return *this;
 
-    delete[] pInventory;
-
-    name = rP.name;
-    occupation = rP.occupation;
-    pInventory = new int[rP.INVENTORY_SIZE];
+    copyPerson(rP);
     return *this;
 }
 
 std::string Person::getName() const
 {
     return name;
+}
+
+int Person::getGold() const
+{
+    return gold;
 }
 
 int Person::getOccupation() const
@@ -44,7 +50,25 @@ void Person::setOccupation(const int &rNewOccupation)
     occupation = rNewOccupation;
 }
 
-int * Person::getInventory() const
+void Person::copyPerson(const Person &rP)
 {
-    return pInventory;
+    name = rP.name;
+    age = rP.age;
+    gender = rP.gender;
+    allignment = rP.allignment;
+    happiness = rP.happiness;
+
+    delete[] pStatusEffects;
+    pStatusEffects = new bool[STATUS_EFFECT_NUM];
+    for (int i = 0; i < STATUS_EFFECT_NUM; i++)
+        pStatusEffects[i] = pStatusEffects[i];
+
+    gold = rP.gold;
+    occupation = rP.occupation;
+
+    delete[] pInventory;
+    pInventory = new int[INVENTORY_SIZE];
+    for (int i = 0; i < INVENTORY_SIZE; i++)
+        pInventory[i] = rP.pInventory[i];
+
 }
